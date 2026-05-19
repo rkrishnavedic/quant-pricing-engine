0f8ca03593df904270d7ce825a37668652ebcfaf
Rahul Krishna <r.krishna.vedic@gmail.com>
2026-05-19 22:09:00 +0530

    enhanced payoff calculation for vectorized prices

--- src/options/payoffs.py
+++ src/options/payoffs.py
@@ -1,5 +1,5 @@
 from abc import ABC, abstractmethod
-
+import numpy as np
 
 class Option(ABC):
     """
@@ -7,7 +7,7 @@ class Option(ABC):
     """
 
     @abstractmethod
-    def payoff(self, S_T: float) -> float:
+    def payoff(self, S_T: float | np.ndarray) -> float | np.ndarray:
         pass
 
 
@@ -16,16 +16,16 @@ class EuropeanCallOption(Option):
     def __init__(self, strike: float):
         self.strike = strike
 
-    def payoff(self, S_T: float) -> float:
-        return max(S_T - self.strike, 0.0)
+    def payoff(self, S_T: float | np.ndarray) -> float | np.ndarray:
+        return np.maximum(S_T - self.strike, 0.0)
 
 
 class EuropeanPutOption(Option):
     def __init__(self, strike: float):
         self.strike = strike
 
-    def payoff(self, S_T: float) -> float:
-        return max(self.strike - S_T, 0.0)
+    def payoff(self, S_T: float | np.ndarray) -> float | np.ndarray:
+        return np.maximum(self.strike - S_T, 0.0)
 
 
 class EuropeanDigitalCallOption(Option):
@@ -33,8 +33,8 @@ class EuropeanDigitalCallOption(Option):
         self.strike = strike
         self.payout = payout
 
-    def payoff(self, S_T: float) -> float:
-        return self.payout if S_T > self.strike else 0.0
+    def payoff(self, S_T: float | np.ndarray) -> float | np.ndarray:
+        return self.payout * np.where(S_T > self.strike, 1.0, 0.0)
 
 
 class EuropeanDigitalPutOption(Option):
@@ -42,5 +42,5 @@ class EuropeanDigitalPutOption(Option):
         self.strike = strike
         self.payout = payout
 
-    def payoff(self, S_T: float) -> float:
-        return self.payout if S_T < self.strike else 0.0
+    def payoff(self, S_T: float | np.ndarray) -> float | np.ndarray:
+        return self.payout * np.where(S_T < self.strike, 1.0, 0.0)
